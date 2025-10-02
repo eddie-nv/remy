08eb0c761388a5e5554c0a62f6f776d5a06cd3ee
Eduardo Nava <ed.nava.valencia@gmail.com>
2025-10-01 20:18:06 -0700

    fix: custom port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const routes = require('./router/routes');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
